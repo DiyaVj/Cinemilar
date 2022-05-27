d9a1ea0d8605b73df96a5828393285fd1b30d9b1
Diya Vj <diya.20it549@rtu.ac.in>
2022-05-27 21:53:48 +0530

    upadeted app

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def recommend(movie):
 
 movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 st.title('Cinemilar')
