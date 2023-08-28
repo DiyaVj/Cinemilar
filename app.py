42fa396b9f10b6e0268b42e841541ab200927247
Diya Vijay <diyavijay2371@gmail.com>
2023-08-28 22:25:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ selected_movie_name = st.selectbox(
 if st.button('Recommend'):
     names,posters = recommend(selected_movie_name)
 
-    col1,col2,col3, col4, col5 = st.beta_columns(5)
+    col1,col2,col3, col4, col5 = st.columns(5)
     with col1:
         st.text(names[0])
         st.image(posters[0])
